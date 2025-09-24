3b4060eaa98551dc6f79efe60364d32cb8d9ecad
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-09-24 20:57:39 +0530

    Add files via upload

---
+++ SORT_S~1.C
@@ -0,0 +1,39 @@
+#include <stdio.h>  
+#include<conio.h>
+void selection(int arr[], int n)
+{
+    int i, j, small,temp;
+
+    for (i = 0; i < n-1; i++)    // One by one move boundary of unsorted subarray
+    {
+        small = i; //minimum element in unsorted array  
+          
+        for (j = i+1; j < n; j++)  
+        if (arr[j] < arr[small])  
+            small = j;  
+// Swap the minimum element with the first element  
+     temp = arr[small];
+    arr[small] = arr[i];  
+    arr[i] = temp;  
+    }  
+}  
+  
+void printArr(int a[], int n) /* function to print the array */  
+{  
+    int i;  
+    for (i = 0; i < n; i++)  
+        printf("%d ", a[i]);  
+}  
+  
+void main()
+{
+    int a[] = { 12, 31, 25, 8, 32, 17 };
+    int n = sizeof(a) / sizeof(a[0]);
+    clrscr();
+    printf("Before sorting array elements are - \n");
+    printArr(a, n);
+    selection(a, n);
+    printf("\nAfter sorting array elements are - \n");
+    printArr(a, n);
+    getch();
+}
